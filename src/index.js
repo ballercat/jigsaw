9dd140983e1d3c43548340052f148d42fa8531a9
Arty Buldauskas <arthurbuldauskas@gmail.com>
2020-01-08 09:51:53 -0500

    test lit html

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import {html, render} from 'lit-html';
+
 const div = document.createElement('div');
 div.id = 'main';
 document.body.appendChild(div);
 
+const Test = () => html`<h1>Hello World!</h1>`;
+render(Test(), div);
 
